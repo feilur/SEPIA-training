2650e195e438748a95bd8dca4a66dcc6d59ed171
HP <hugo.proux@live.fr>
2021-10-26 21:01:19 +0200

    add settings form structure

--- js/application/modalSettings.js
+++ js/application/modalSettings.js
@@ -1,8 +1,16 @@
 $(document).ready(function() {
 
-
+    $("#btnSaveSettings").on('click', function(){
+        saveSettings();
+    });
 });
 
 function openModalSettings(){
+    //Load all field with jsonSettings
+
     $("#modalSettings").modal('show');
+}
+
+function saveSettings(){
+    fShowError("Not implemented yet");
 }
